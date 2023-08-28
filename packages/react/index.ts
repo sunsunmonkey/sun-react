136a80e4c73806cdeaa74ed560e894d95492adcc
sunsunmonkey <2933201543@qq.com>
2023-08-28 20:12:49 +0800

    feat: jsx link

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,6 +1,6 @@
-import { jsx } from './src/jsx';
+import { jsxDEV } from './src/jsx';
 
 export default {
 	version: '0.0.0',
-	createElement: jsx
+	createElement: jsxDEV
 };
--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -62,4 +62,30 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDev = jsx;
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
